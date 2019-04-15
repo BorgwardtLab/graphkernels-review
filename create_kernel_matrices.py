59974a2b86f3f6cee22bdca85cf0c85a6e40278d
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-04-15 16:49:59 +0200

    Loading all graphs

--- create_kernel_matrices.py
+++ create_kernel_matrices.py
@@ -11,10 +11,12 @@ import sys
 import graphkernels.kernels as gk
 import igraph as ig
 
+from tqdm import tqdm
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('INPUT', type=str, help='Input directory')
+    parser.add_argument('FILE', nargs='+', type=str, help='Input file(s)')
     parser.add_argument(
         '-f', '--force', action='store_true',
         help='If specified, overwrites data'
@@ -28,6 +30,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    logging.basicConfig(
+        level=logging.INFO,
+        format=None
+    )
+
     # Check if the output directory already contains some files. If so,
     # do not run the script unless `--force` has been specified.
     if os.path.exists(args.output) and not args.force:
@@ -37,3 +44,10 @@ is specified.
         ''')
 
         sys.exit(-1)
+
+    logging.info('Loading graphs...')
+
+    graphs = [
+        ig.read(filename, format='picklez') for filename in
+            tqdm(args.FILE, desc='File')
+    ]
