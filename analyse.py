42b6313bc5e02a25fd263f1fce3b3a0784117f36
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-04-29 16:29:32 +0200

    Made formatted output more beautiful

--- analyse.py
+++ analyse.py
@@ -50,21 +50,21 @@ def collate_performance_measure(measure, data, aggregate='mean'):
         results_per_fold = collections.defaultdict(list)
 
         for fold in data_per_iteration['folds']:
-          data_per_fold = data_per_iteration['folds'][fold]
+            data_per_fold = data_per_iteration['folds'][fold]
 
-          # Store the desired measure per kernel and per fold; the
-          # result is a list of values for each iteration.
-          for kernel in data_per_fold['kernels']:
-              data_per_kernel = data_per_fold['kernels'][kernel]
-              value = data_per_kernel[measure]
-              results_per_fold[kernel].append(value)
+            # Store the desired measure per kernel and per fold; the
+            # result is a list of values for each iteration.
+            for kernel in data_per_fold['kernels']:
+                data_per_kernel = data_per_fold['kernels'][kernel]
+                value = data_per_kernel[measure]
+                results_per_fold[kernel].append(value)
 
         # Aggregate values over all folds to obtain *one* value per
         # iteration because that is what we need to properly report
         # everything.
         for kernel, values in results_per_fold.items():
-          aggregated_value = aggregate_fn(values)
-          results[kernel].append(aggregated_value)
+            aggregated_value = aggregate_fn(values)
+            results[kernel].append(aggregated_value)
 
     return results
 
@@ -84,10 +84,18 @@ if __name__ == '__main__':
     accuracies = collate_performance_measure('accuracy', data)
 
     df = pd.DataFrame.from_dict(accuracies, orient='index')
+    df = df * 100
     mean = df.mean(axis=1)
     std = df.std(axis=1)
 
     df['mean'] = mean
     df['std'] = std
-    
-    print(tabulate.tabulate(df, tablefmt='github', headers='keys'))
+
+    print(
+      tabulate.tabulate(
+        df,
+        tablefmt='github',
+        floatfmt='2.2f',
+        headers='keys',
+      )
+    )
