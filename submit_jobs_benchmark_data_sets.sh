27d3fb76f511cc5531251ac83ca0ed5bcba4a28b
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-06-07 17:55:08 +0200

    Improved job submission script

---
+++ submit_jobs_benchmark_data_sets.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# submit_jobs_benchmark_data_sets.sh: main controller for submitting
+# jobs to Euler or another cluster that is equipped with LFS. Notice
+# that this variant only uses some 'recognised' benchmark data sets,
+# making it easier to obtain results.
+
+DATA_SETS=(BZR BZR_MD COLLAB DD ENZYMES MUTAG NCI1 NCI109 PROTEINS PTC_FM PTC_FR PTC_MM PTC_MR)
+
+# TODO: need more kernels here...
+KERNELS=(WL GL SP VH EH)
+
+for DATA_SET in "${DATA_SETS[@]}"; do
+  for KERNEL in "${KERNELS[@]}"; do
+    bsub "run.sh ${DATA_SET} ${KERNEL}"
+  done
+done
