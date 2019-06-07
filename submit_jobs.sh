3b2d70183128af0d0c154dafea276732e032011d
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-06-07 16:00:03 +0200

    Updated basic job submission script

---
+++ submit_jobs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# submit_jobs.sh: main controller for submitting jobs to Euler or
+# another cluster that is equipped with LFS.
+
+DATA_SETS=(AIDS BZR BZR_MD COIL-DEL COIL-RAG COLLAB COX2 COX2_MD DBLP_v1 DD DHFR DHFR_MD ENZYMES ER_MD FIRSTMM_DB FRANKENSTEIN IMDB-BINARY IMDB-MULTI KKI Letter-high Letter-low Letter-med MSRC_21 MSRC_21C MSRC_9 MUTAG Mutagenicity NCI1 NCI109 OHSU PROTEINS PROTEINS_full PTC_FM PTC_FR PTC_MM PTC_MR Peking_1 README.md REDDIT-BINARY REDDIT-MULTI-12K REDDIT-MULTI-5K SYNTHETIC SYNTHETICnew Synthie Tox21_AHR Tox21_AR Tox21_AR-LBD Tox21_ARE Tox21_ATAD5 Tox21_ER Tox21_ER_LBD Tox21_HSE Tox21_MMP Tox21_PPAR-gamma Tox21_aromatase Tox21_p53)
+
+for DATA_SET in DATA_SETS; do
+  echo $DATA_SET
+done
