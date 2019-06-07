4771d3706752fb2495c0c5e0e3289879f73ab8a5
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-06-07 11:05:34 +0200

    Added basic job running script

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# run.sh: basic job running script for clusters with LSF support. Given
+# a data set name and an algorithm, will create a kernel matrix, and do
+# the training of said matrix.
+#
+# All results will be stored in `results_run`.
+#
+# Usage:
+#   run.sh DATA ALGORITHM
+#
+# Parameters:
+#   DATA: Essentially, just a folder name in `data`. All files are taken
+#   from there.
+#
+#   ALGORITHM: An abbreviation of one of the algorithms to run. One of
+#   these days, I will document all of them in the repo.
+
+python3 ../src/create_kernel_matrices.py 
+echo "My second parameter is $2"
