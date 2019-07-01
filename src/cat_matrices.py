9ea92155ca9b59f028d69e673d4d68dfa2ddc16c
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-07-01 15:35:04 +0200

    Preparing `cat` command for matrices

---
+++ src/cat_matrices.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+#
+# cat_matrices.py: script that concatenates a set of text-based matrices
+# and stores them in `.npz` format with proper lablels. This is required
+# in order to provide input from *other* graph kernels.
