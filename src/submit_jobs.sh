d77e66c3a4bf9fae6f94903120af4893974a6f16
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-07-02 11:29:11 +0200

    Restructured data sets to account for size

--- src/submit_jobs.sh
+++ src/submit_jobs.sh
@@ -3,7 +3,7 @@
 # submit_jobs.sh: main controller for submitting jobs to Euler or
 # another cluster that is equipped with LFS.
 
-DATA_SETS=(AIDS BZR BZR_MD COIL-DEL COIL-RAG COX2 COX2_MD DBLP_v1 DD DHFR DHFR_MD ENZYMES ER_MD FIRSTMM_DB FRANKENSTEIN IMDB-BINARY IMDB-MULTI KKI Letter-high Letter-low Letter-med MSRC_21 MSRC_21C MSRC_9 MUTAG Mutagenicity OHSU PROTEINS PROTEINS_full PTC_FM PTC_FR PTC_MM PTC_MR Peking_1 REDDIT-BINARY REDDIT-MULTI-12K REDDIT-MULTI-5K SYNTHETIC SYNTHETICnew Synthie)
+DATA_SETS=(AIDS BZR BZR_MD COX2 COX2_MD DBLP_v1 DD DHFR DHFR_MD ENZYMES ER_MD FIRSTMM_DB IMDB-BINARY IMDB-MULTI KKI Letter-high Letter-low Letter-med MSRC_21 MSRC_21C MSRC_9 MUTAG Mutagenicity OHSU PROTEINS PROTEINS_full PTC_FM PTC_FR PTC_MM PTC_MR Peking_1 REDDIT-BINARY SYNTHETIC SYNTHETICnew Synthie)
 
 if [ -z ${KERNELS+x} ]; then
   KERNELS=(EH GL MLG SP VH WL WLOA)
@@ -18,7 +18,7 @@ done
 # Handle large data sets by submitting them to an even longer queue
 # where they will hopefully be executed at some point.
 
-LARGE_DATA_SETS=(COLLAB NCI1 NCI109)
+LARGE_DATA_SETS=(COIL-DEL COIL-RAG COLLAB FRANKENSTEIN NCI1 NCI109 REDDIT-MULTI-5K REDDIT-MULTI-12K)
 
 for DATA_SET in "${LARGE_DATA_SETS[@]}"; do
   for KERNEL in "${KERNELS[@]}"; do
