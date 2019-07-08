053daa74e89ad039203b7953aeac72ad76380f24
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-07-08 13:34:19 +0200

    Fixed job submission script

--- src/submit_jobs.sh
+++ src/submit_jobs.sh
@@ -22,7 +22,7 @@ LARGE_DATA_SETS=(COIL-DEL COIL-RAG COLLAB FRANKENSTEIN NCI1 NCI109 REDDIT-MULTI-
 
 for DATA_SET in "${LARGE_DATA_SETS[@]}"; do
   for KERNEL in "${KERNELS[@]}"; do
-    bsub -W 119:59 -R "rusage[mem=64000]" "./run.sh ${DATA_SET} ${KERNEL} -I 1000"
+    bsub -W 119:59 -R "rusage[mem=64000]" "./run.sh ${DATA_SET} ${KERNEL} -I 10000"
   done
 done
 
