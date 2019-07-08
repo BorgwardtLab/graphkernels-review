56d9a235ffd3f80802eddbc01e6879bef0decae4
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-07-08 08:14:49 +0200

    Added support for maximum number of iterations

--- src/run.sh
+++ src/run.sh
@@ -7,7 +7,7 @@
 # All results will be stored in `results_run`.
 #
 # Usage:
-#   run.sh DATA ALGORITHM
+#   run.sh DATA ALGORITHM [MAX_ITERATIONS]
 #
 # Parameters:
 #   DATA: Essentially, just a folder name in `data`. All files are taken
@@ -15,6 +15,16 @@
 #
 #   ALGORITHM: An abbreviation of one of the algorithms to run. One of
 #   these days, I will document all of them in the repo.
+#
+#   MAX_ITERATIONS: An optional argument indicating the maximum number
+#   of iterations for the SVM.
+
 
 python3 ../src/create_kernel_matrices.py -a $2 -o ../matrices/$1 ../data/$1/*.pickle
-python3 ../src/train.py ../matrices/$1/$2.npz -n $1 -o ../results/$1_$2.json
+
+if [ "$3" ]; then
+    python3 ../src/train.py ../matrices/$1/$2.npz -n $1 -o ../results/$1_$2.json
+  else
+    python3 ../src/train.py ../matrices/$1/$2.npz -n $1 -o ../results/$1_$2.json -I $3
+fi
+
