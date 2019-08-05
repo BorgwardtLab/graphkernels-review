5ea336ca008c8617b269751859bb4602a78efd37
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-08-05 15:05:12 +0200

    Added more statistics for each graph

--- src/show_statistics.py
+++ src/show_statistics.py
@@ -15,21 +15,59 @@ import glob
 import os
 
 import igraph as ig
+import numpy as np
 import pandas as pd
 
 from tqdm import tqdm
 
 
-def get_statistics(graphs, name):
+def get_statistics(graphs):
     '''
     Calculates statistics for a given data set of graphs and returns a
     `dict` data structure for further processing.
 
     :param graphs: Graph data set
-    :param name: Name of data set
     '''
 
-    return {}
+    n_graphs = len(graphs)
+
+    n_classes = len(set([g['label'] for g in graphs]))
+
+    n_nodes = [len(g.vs) for g in graphs]
+    n_edges = [len(g.es) for g in graphs]
+
+    has_node_labels = ['label' in g.vs.attributes() for g in graphs]
+    has_edge_labels = ['label' in g.es.attributes() for g in graphs]
+
+    dim_node_attributes = None
+    dim_edge_attributes = None
+
+    has_node_attributes = ['attribute' in g.vs.attributes() for g in graphs]
+    has_edge_attributes = ['attribute' in g.es.attributes() for g in graphs]
+
+    if np.all(has_node_attributes):
+
+        for g in graphs:
+            print(g.vs['attribute'])
+        dim_node_attributes = np.mean(
+            [len(g.vs['attribute']) for g in graphs]
+        )
+
+    if np.all(has_edge_attributes):
+        dim_edge_attributes = np.mean(
+            [len(g.es['attribute']) for g in graphs]
+        )
+
+    return {
+        'n_graphs': n_graphs,
+        'n_classes': n_classes,
+        'avg_n_nodes': f'{np.mean(n_nodes):.2f}',
+        'avg_n_edges': f'{np.mean(n_edges):.2f}',
+        'has_node_labels': np.all(has_node_labels),
+        'has_edge_labels': np.all(has_edge_labels),
+        'dim_node_attributes': dim_node_attributes,
+        'dim_edge_attributes': dim_edge_attributes,
+    }
 
 
 if __name__ == '__main__':
@@ -64,6 +102,11 @@ if __name__ == '__main__':
 
             # Use the directory name as the data set name
             name = directory
-            rows.append(get_statistics(graphs, name))
+            rows.append({name: get_statistics(graphs)})
+
+            # FIXME: Handle more than one data set :)
+            break
 
         break
+
+    print(rows)
