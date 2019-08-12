10c65ca0e29f29d6d8693abc50c26e9c46493f16
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-08-12 14:04:36 +0200

    Started writing script for dendrogram creation
    Added basic dendrogram based on the accuracy values on the benchmark
    data sets.

---
+++ src/make_dendrograms.R
@@ -0,0 +1,30 @@
+data <- read.table(
+  '../results/Accuracies.csv',
+  sep=',',
+  header=TRUE,
+  row.names=1
+)
+
+# Graphlet kernel
+rownames(data) <- gsub("GL", "Graphlet", rownames(data))
+
+# Hash Graph Kernels
+rownames(data) <- gsub("HGKSP_seed0", "HGK-SP", rownames(data))
+rownames(data) <- gsub("HGKWL_seed0", "HGK-WL", rownames(data))
+
+# Histogram kernels
+rownames(data) <- gsub("EH", "Histogram (E)", rownames(data))
+rownames(data) <- gsub("VH", "Histogram (V)", rownames(data))
+
+# Weisfeiler--Lehman Optimal Assignment kernel
+rownames(data) <- gsub("WLOA", "WL-OA", rownames(data))
+
+distances.accuracy <- dist(data, method='euclidean')
+hc <- hclust(distances.accuracy)
+hcd <- as.dendrogram(hc)
+
+pdf('Dendrogram_accuracies.pdf')
+
+plot(hcd)
+
+dev.off()
