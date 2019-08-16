Fixed normalisation procedure for certain kernel matrices
It turns out that *some* kernels give rise to zero values on the
diagonal, which is of course frowned upon when normalising. This
was preventing certain graph kernels from running.

src/train.py
@@ -44,7 +44,14 @@ def normalize(matrix):
     :return: Normalized matrix
     '''
 
-    k = 1.0 / np.sqrt(np.diagonal(matrix))
+    # Ensures that only non-zero entries will be subjected to the
+    # normalisation procedure. The remaining entries will be kept
+    # at zero. This prevents 'NaN' values from cropping up.
+    mask = np.diagonal(matrix) != 0
+    n = len(np.diagonal(matrix)) 
+    k = np.zeros((n, ))
+    k[mask] = 1.0 / np.sqrt(np.diagonal(matrix)[mask])
+
     return np.multiply(matrix, np.outer(k, k))
 
 