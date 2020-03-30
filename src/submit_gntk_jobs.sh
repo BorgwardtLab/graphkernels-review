f22f8be5f81b0824007ae3a266873c022a108740
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-30 08:22:52 +0200

    Modified runtime for GNTK jobs

--- src/submit_gntk_jobs.sh
+++ src/submit_gntk_jobs.sh
@@ -3,7 +3,7 @@
 DATA_SETS=(MUTAG PROTEINS IMDB-BINARY IMDB-MULTI PTC_MR NCI1)
 
 if [ -z ${KERNELS+x} ]; then
-  KERNELS=(EH HGKSP_seed0 HGKWL_seed0 MLG MP SP VH WL WLOA)
+  KERNELS=(EH HGKSP_seed0 HGKWL_seed0 MLG MP VH WL WLOA)
 fi
 
 # Check whether the number of iterations has been set from the outside.
@@ -12,17 +12,11 @@ if [ -z ${N_ITERS+x} ]; then
   N_ITERS=500
 fi
 
+MEMORY=64000
+TIME=119:59
+
 for DATA_SET in "${DATA_SETS[@]}"; do
   for KERNEL in "${KERNELS[@]}"; do
-
-    # The SP kernel requires more memory and more time...
-    MEMORY=64000
-    TIME=23:59
-    if [ ${KERNEL} = "SP" ]; then
-      MEMORY=128000
-      TIME=119:59
-    fi
-
     bsub -W ${TIME} -R "rusage[mem=${MEMORY}]" "./run_directly.sh ${DATA_SET} ${KERNEL}"
   done
 done
